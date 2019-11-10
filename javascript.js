e61100d0b4ea7f4369f62f6cd57e4d92aca85c3d
Lynn O'Neill <loneill@afgaz.com>
2019-11-10 15:26:59 -0700

    used media queries to make page responsive

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
-<script type="text/javascript">
 
-var kd = 
-alert("Welcome to the most awesome password generator that ever was!  Are you ready to create your new password?")
+    var specialChar = ["!", ",", "#", "$", "%", "&", "*", "+", "<", "=", ">", "?", "@", "~"]
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        // alert("Welcome to the world's most awesome password generator!  Are you ready to create your new password?");
+
+
+        // Need to figure out how to place this if/else statement into a loop.
+        // var numberInput = prompt("How many characters would you like your password to have? Pick a number between 8 and 128");
+
+        // if(numberInput < 8 || numberInput > 128) {
+        //   prompt("Try again! How many characters would you like your password to have?  Pick a number between 8 and 128")
+        // }
+        // else if(numberInput > 7 && numberInput < 129){
+        //   passwordLength = numberInput
+        // }
 
 
 
