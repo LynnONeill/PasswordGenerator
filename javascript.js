8897a68124b11c2ae167110ffe801b80e6320363
Lynn O'Neill <loneill@afgaz.com>
2019-11-15 12:34:19 -0700

    Re-worked javascript to interact with html

--- javascript.js
+++ javascript.js
@@ -1,17 +1,62 @@
 
-   
+var upper = ["ABCEDFGHIJKLMNOPQRSTUVWXYZ"];
+var lower = ["abcdefghijklmnopqrstuvwxyz"];
+var alpha = ["0123456789"];
+var special = ["!,#$%^&*()~+=<>|?"];
+var charSet = "";
+// var fullPass = "Your password is ";
 
-   
+var confAlpha = document.getElementById("numbers");
+var confLower = document.getElementById("lowers");
+var confUpper = document.getElementById("uppers");
+var confSpecials = document.getElementById("specials");
+var password ="";
 
-        // Need to figure out how to place this if/else statement into a loop.
-        // var numberInput = prompt("How many characters would you like your password to have? Pick a number between 8 and 128");
+var charNo = document.getElementById("charBox");
+console.log(confUpper.checked);
+console.log(confSpecials.checked);
+function generate() {
 
-        // if(numberInput < 8 || numberInput > 128) {
-        //   prompt("Try again! How many characters would you like your password to have?  Pick a number between 8 and 128")
-        // }
-        // else if(numberInput > 7 && numberInput < 129){
-        //   passwordLength = numberInput
-        // }
+  if (confAlpha.checked == true) {
+    charSet += alpha;
+    console.log(charSet);
+  }
+  if (confLower.checked == true) {
+    charSet += lower;
+    console.log(charSet);
+  }
+
+  if (confUpper.checked == true) {
+    charSet += upper;
+    console.log(charSet);
+  }
+
+  if (confSpecials.checked == true) {
+    charSet += special;
+    console.log(charSet);
+  }
+    for (var i = 0; i < charNo.value; i++) {
+
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+      console.log(password);
+    }
+
+  return document.getElementById("password").innerText = password;
+  }
+  var copyText = document.getElementById("copy");
+  copyText.addEventListener("click" , copy);
+
+  function copy(e) {
+    document.querySelector("#password").select();
+    document.execCommand("Copy");
+    e.preventDefault();
+  }
+
+
+
+
+
+
 
 
 
